0dbea605ade54415e626dfa990d5e17f21684250
whyfarer <whyfarer@gmail.com>
2021-01-05 09:58:41 -0800

    adding all manjaro dotfiles I have been using ...

---
+++ .config/polybar/scripts/launch_multimonitor.sh
@@ -0,0 +1,48 @@
+#/!bin/bash
+
+# Detect if secondary monitor is connected, if so, add a specific bar and move tray to it
+# Else, keep tray on main monitor
+# see https://github.com/polybar/polybar/issues/763
+
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
+
+
+# below allows for different bars in different places and specification of the tray
+
+# outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
+# set -- $outputs
+# tray_output=$1
+
+# replace <main> with different polybar configs to use different bars on different monitors
+# for m in $outputs; do
+#     if [ $m == $1 ]
+#     then
+#     	MONITOR1=$m polybar --reload main -c ~/.config/polybar/config &
+#     elif [ $m == $2 ]
+#     then
+#     	tray_output=$m
+#     	MONITOR2=$m polybar --reload main -c ~/.config/polybar/config &
+#     else
+#     	MONITOR1=$m polybar --reload main -c ~/.config/polybar/config &
+#     fi
+# done
+
+# for m in $outputs; do
+#     export MONITOR1=$1
+#     export MONITOR2=$2
+#     export TRAY_POSITION=none
+#     if [[ $m == $tray_output ]]; then
+#         TRAY_POSITION=right
+#     fi
+# done
